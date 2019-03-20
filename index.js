555928c0375c295a03e8f0947025a1e86a9d399c
a15davidrp <44055942+a15davidrp@users.noreply.github.com>
2019-03-20 13:47:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,16 @@ var app = require('./app')
 var mongoose = require('mongoose');
 var port = process.env.PORT || 3678
 
+bd = 'mongodb+srv://root:root@clusterprueba-dpivw.mongodb.net/test?retryWrites=true'
+
+mongoose.connect(bd, (err, res) => {
+    if (err) {
+        throw err;
+    } else {
+        console.log('Conexión a mongodb correcta.')
+        app.listen(port, () => {
+            console.log("API REST funcionando en http://localhost:" + port);
+        });
+    }
+});
 
